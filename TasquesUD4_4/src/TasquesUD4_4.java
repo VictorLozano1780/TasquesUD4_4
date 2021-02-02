c2a888f4a2855c0d40cd1838fe55ea932711035c
Víctor <victor.lozano1780@gmail.com>
2021-02-02 16:47:01 +0100

    Acabado

---
+++ TasquesUD4_4/src/TasquesUD4_4.java
@@ -0,0 +1,15 @@
+
+public class TasquesUD4_4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		
+		System.out.println("Valor inicial N = " + n);
+		n = n+77;
+		System.out.println("N + 77= " + n);
+		n = n-3;
+		System.out.println("N - 3= " + n);
+		n = n*2;
+		System.out.println("N * 2= " + n);
+	}
+}
